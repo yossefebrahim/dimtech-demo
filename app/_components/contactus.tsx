bfa51c52056415bdecc78f80158081b5d2306a27
ahmed mohamed <ahmed.mohamed@afaqy.com>
2025-04-20 16:22:45 +0200

    change border color

--- app/_components/contactus.tsx
+++ app/_components/contactus.tsx
@@ -297,7 +297,7 @@ const Contactus = () => {
                 setSelectedCountry(key as string);
               }}
               classNames={{
-                cursor: "w-full bg-primary text-white  bg-[#F780AC]",
+                cursor: "w-full bg-primary text-white  bg-[#681EE0]",
                 base: "text-white",
                 tabList: "flex justify-between w-full ",
                 tab: "h-[50px]  data-[hover=true]:bg-[#493D99] data-[hover=true]:opacity-100 data-[hover=true]:rounded-sm transition-colors duration-200",
